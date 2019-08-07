9fdc4608fbcfe214b1a3482680f3f92d03a8df44
Nadia Murchison <nadiamurchison@gmail.com>
2019-08-07 19:02:33 -0400

    shufflecardcomplete

--- public/main.js
+++ public/main.js
@@ -1,7 +1,46 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+const playingCardDeckType = ["Hearts", "Spades", "Clubs", "Diamonds"];
+const value = [
+  "Ace",
+  "King",
+  "Queen",
+  "Jack",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10"
+];
 
-document.addEventListener('DOMContentLoaded', main)
+const deck = [];
+
+const createDeckShuffle = () => {
+  console.log("Shuffle!");
+  for (let typeIndex = 0; typeIndex < playingCardDeckType.length; typeIndex++) {
+    for (let valuesIndex = 0; valuesIndex < value.length; valuesIndex++) {
+      //store values of cards into variables
+      const newType = playingCardDeckType[typeIndex];
+      const newValue = value[valuesIndex];
+
+      //combines both arrays to create a full deck
+      deck.push(`${newValue} ${"of"} ${newType}`);
+    }
+  }
+
+  for (let i = deck.length - 1; i >= 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+  }
+};
+const deal = () => {
+  document.querySelector(".card").textContent = deck.pop();
+  console.log(deck.length);
+};
+
+document.querySelector(".pull-card").addEventListener("click", deal);
+document.addEventListener("DOMContentLoaded", createDeckShuffle);
