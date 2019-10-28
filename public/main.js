18fb1d49ea8d78caba0c9ea01ef11ad67d4ecc24
Nadia Murchison <nadiamurchison@gmail.com>
2019-10-28 16:20:54 -0400

    Initial Commit Before Refactoring

--- public/main.js
+++ public/main.js
@@ -1,106 +1,106 @@
 const state = {
-  suits: ["Hearts", "Spades", "Clubs", "Diamonds"],
+  suits: ['Hearts', 'Spades', 'Clubs', 'Diamonds'],
   ranks: [
-    { name: "Ace", value: 11 },
-    { name: "King", value: 10 },
-    { name: "Queen", value: 10 },
-    { name: "Jack", value: 10 },
-    { name: "2", value: 2 },
-    { name: "3", value: 3 },
-    { name: "4", value: 4 },
-    { name: "5", value: 5 },
-    { name: "6", value: 6 },
-    { name: "7", value: 7 },
-    { name: "8", value: 8 },
-    { name: "9", value: 9 },
-    { name: "10", value: 10 }
+    { name: 'Ace', value: 11 },
+    { name: 'King', value: 10 },
+    { name: 'Queen', value: 10 },
+    { name: 'Jack', value: 10 },
+    { name: '2', value: 2 },
+    { name: '3', value: 3 },
+    { name: '4', value: 4 },
+    { name: '5', value: 5 },
+    { name: '6', value: 6 },
+    { name: '7', value: 7 },
+    { name: '8', value: 8 },
+    { name: '9', value: 9 },
+    { name: '10', value: 10 }
   ],
 
   deck: [],
   playerHand: [],
   dealerHand: []
-};
+}
 
 const main = () => {
-  createDeck();
-  shuffleDeck();
-  dealPlayerHand();
-  dealDealerHand();
-  playerHandTotal();
-  dealerHandTotal();
-};
+  createDeck()
+  shuffleDeck()
+  dealPlayerHand()
+  dealDealerHand()
+  playerHandTotal()
+  dealerHandTotal()
+}
 
 const createDeck = () => {
   for (let suitIndex = 0; suitIndex < state.suits.length; suitIndex++) {
-    const suit = state.suits[suitIndex];
+    const suit = state.suits[suitIndex]
     for (let rankIndex = 0; rankIndex < state.ranks.length; rankIndex++) {
-      const rank = state.ranks[rankIndex];
+      const rank = state.ranks[rankIndex]
       state.deck.push({
         rank: rank.name,
         value: rank.value,
         suit: suit
-      });
+      })
     }
   }
-  console.log(state.deck);
-};
+  console.log(state.deck)
+}
 
 const shuffleDeck = () => {
-  console.log("Shuffling!");
+  console.log('Shuffling!')
   for (let i = state.deck.length - 1; i >= 0; i--) {
-    const random = Math.floor(Math.random() * i);
-    const temp = state.deck[random];
-    state.deck[random] = state.deck[i];
-    state.deck[i] = temp;
+    const random = Math.floor(Math.random() * i)
+    const temp = state.deck[random]
+    state.deck[random] = state.deck[i]
+    state.deck[i] = temp
   }
-  console.log(state.deck);
-};
+  console.log(state.deck)
+}
 
-//Automatic Deal Card Function - Player
+// Automatic Deal Card Function - Player
 
 const dealPlayerHand = () => {
   for (let i = 0; i < 2; i++) {
-    const dealtCard = state.deck.pop();
-    state.playerHand.push(dealtCard);
-    let cardElement = document.createElement("img");
+    const dealtCard = state.deck.pop()
+    state.playerHand.push(dealtCard)
+    const cardElement = document.createElement('img')
     cardElement.src =
-      "/images/" +
+      '/images/' +
       state.playerHand[i].rank +
-      "_of_" +
+      '_of_' +
       state.playerHand[i].suit +
-      ".svg";
-    document.querySelector(".card").appendChild(cardElement);
+      '.svg'
+    document.querySelector('.card').appendChild(cardElement)
   }
-  console.log("Player pulled card!", state.deck.length);
-};
+  console.log('Player pulled card!', state.deck.length)
+}
 
-//Automatic Deal Card Function - Dealer
+// Automatic Deal Card Function - Dealer
 
 const dealDealerHand = () => {
   for (let i = 0; i < 2; i++) {
-    const dealtCard = state.deck.pop();
-    state.dealerHand.push(dealtCard);
-    let cardElement = document.createElement("img");
+    const dealtCard = state.deck.pop()
+    state.dealerHand.push(dealtCard)
+    const cardElement = document.createElement('img')
     cardElement.src =
-      "/images/" +
+      '/images/' +
       state.dealerHand[i].rank +
-      "_of_" +
+      '_of_' +
       state.dealerHand[i].suit +
-      ".svg";
-    document.querySelector(".card-2").appendChild(cardElement);
+      '.svg'
+    document.querySelector('.card-2').appendChild(cardElement)
     // document.querySelector(".card-2").textContent = `${dealtCard.rank} of ${
     //   dealtCard.suit
     // }`;]
   }
-  console.log("Dealer pulled card!", state.deck.length);
-};
+  console.log('Dealer pulled card!', state.deck.length)
+}
 
 const playerHandTotal = () => {
   const playerTotal = state.playerHand.reduce((runningTotal, card) => {
-    return runningTotal + card.value;
-  }, 0);
+    return runningTotal + card.value
+  }, 0)
 
-  document.querySelector(".total").textContent = playerTotal;
+  document.querySelector('.total').textContent = playerTotal
 
   // for (let i = 0; i < state.playerHand.length; i++) {
   //   const card = state.playerHand[i];
@@ -112,86 +112,97 @@ const playerHandTotal = () => {
   //   document.querySelector(".total").textContent = playerTotal;
   // }
   if (playerTotal > 21) {
-    console.log("Player loses!");
-    document.querySelector(".player").textContent = "Player loses!";
-    document.querySelector(".player-hit").disabled = true;
-    document.querySelector(".stand").disabled = true;
+    console.log('Player loses!')
+    document.querySelector('.player').textContent = 'Player loses!'
+    document.querySelector('.player-hit').disabled = true
+    document.querySelector('.stand').disabled = true
   }
 
   if (playerTotal == 21) {
-    console.log("Player wins!");
-    document.querySelector(".player").textContent = "Player wins!";
-    document.querySelector(".player-hit").disabled = true;
-    document.querySelector(".stand").disabled = true;
+    console.log('Player wins!')
+    document.querySelector('.player').textContent = 'Player wins!'
+    document.querySelector('.player-hit').disabled = true
+    document.querySelector('.stand').disabled = true
   }
-  console.log("Player hand total", playerTotal);
-};
+  console.log('Player hand total', playerTotal)
+}
 
 const hitPlayerHand = () => {
   for (let i = 0; i < 1; i++) {
-    let hitCard = state.deck.pop();
-    state.playerHand.push(hitCard);
-    playerHandTotal();
-    let cardElement = document.createElement("img");
+    const hitCard = state.deck.pop()
+    state.playerHand.push(hitCard)
+    playerHandTotal()
+    const cardElement = document.createElement('img')
     cardElement.src =
-      "/images/" +
+      '/images/' +
       state.dealerHand[i].rank +
-      "_of_" +
+      '_of_' +
       state.dealerHand[i].suit +
-      ".svg";
-    document.querySelector(".card").appendChild(cardElement);
+      '.svg'
+    document.querySelector('.card').appendChild(cardElement)
   }
-  console.log("Player hit card!", state.deck.length);
-};
+  console.log('Player hit card!', state.deck.length)
+}
 
 const dealerHandTotal = () => {
-  let dealerTotal;
+  let dealerTotal
   for (let i = 0; i < state.dealerHand.length; i++) {
-    const card = state.dealerHand[i];
+    const card = state.dealerHand[i]
     if (dealerTotal) {
-      dealerTotal += card.value;
+      dealerTotal += card.value
     } else {
-      dealerTotal = card.value;
+      dealerTotal = card.value
     }
-    document.querySelector(".total-2").textContent = dealerTotal;
+    document.querySelector('.total-2').textContent = dealerTotal
   }
   if (dealerTotal > 21) {
-    console.log("Player loses!");
-    document.querySelector(".dealer").textContent = "Dealer loses!";
+    console.log('Player loses!')
+    document.querySelector('.dealer').textContent = 'Dealer loses!'
+    document.querySelector('.player-hit').disabled = true
+    document.querySelector('.stand').disabled = true
   }
 
   if (dealerTotal == 21) {
-    console.log("Player wins!");
-    document.querySelector(".dealer").textContent = "Dealer wins!";
+    console.log('Player wins!')
+    document.querySelector('.dealer').textContent = 'Dealer wins!'
+    document.querySelector('.player-hit').disabled = true
+    document.querySelector('.stand').disabled = true
   }
-  console.log("Dealer hand total", dealerTotal);
-};
+  console.log('Dealer hand total', dealerTotal)
+}
 
-//Stand Function
+// Stand Function
 
 const stand = () => {
   for (let i = 0; i < 1; i++) {
-    let standCard = state.deck.pop();
-    state.dealerHand.push(standCard);
-    dealerHandTotal();
-    let cardElement = document.createElement("img");
+    const standCard = state.deck.pop()
+    state.dealerHand.push(standCard)
+    dealerHandTotal()
+    const cardElement = document.createElement('img')
     cardElement.src =
-      "/images/" +
+      '/images/' +
       state.dealerHand[i].rank +
-      "_of_" +
+      '_of_' +
       state.dealerHand[i].suit +
-      ".svg";
-    document.querySelector(".card-2").appendChild(cardElement);
+      '.svg'
+    document.querySelector('.card-2').appendChild(cardElement)
 
     if (dealerHandTotal < 16) {
-      state.deck.pop();
+      state.deck.pop()
     }
+    if (dealerHandTotal > 16) {
+      document.querySelector('.dealer').textContent = 'Dealer wins!'
+      document.querySelector('.player-hit').disabled = true
+      document.querySelector('.stand').disabled = true
+      console.log('Player chose stand and lost!')
+    }
+
     // if (state.rank.value > 21) {
     //   console.log("Dealer loses!");
     //   document.querySelector(".player-hit").disabled = true;
     //   document.querySelector(".stand").disabled = true;
   }
-};
+}
 
 // const deal = () => {
 //   document.querySelector(".card").textContent = state.deck.pop();
@@ -205,9 +216,9 @@ const stand = () => {
 
 // document.querySelector(".pull-card").addEventListener("click", dealPlayerHand);
 
-//Other Possibility:
+// Other Possibility:
 // let card.src = document.createElement("./images/cards/${suit}_of_${rank}.svg")
 
-document.querySelector(".stand").addEventListener("click", stand);
-document.querySelector(".player-hit").addEventListener("click", hitPlayerHand);
-document.addEventListener("DOMContentLoaded", main);
+document.querySelector('.stand').addEventListener('click', stand)
+document.querySelector('.player-hit').addEventListener('click', hitPlayerHand)
+document.addEventListener('DOMContentLoaded', main)
